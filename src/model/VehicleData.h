7bec7fbf32e49b0a20bc4ff6e36af9af4dc84dbe
alekseevya <yrasik1986@bk.ru>
2026-08-23 15:30:18 +0300

    add comment in VehicleData

--- src/model/VehicleData.h
+++ src/model/VehicleData.h
@@ -2,20 +2,23 @@
 
 #include <cstdint>
 
+/**
+ * @brief Структура данных автомобиля, получаемых с CAN-шины
+ */
 struct VehicleData
 {
-    double speed = 0.0;
-    std::uint16_t rpm = 0;
+    double speed = 0.0;                      ///< Скорость, км/ч
+    std::uint16_t rpm = 0;                   ///< Обороты двигателя, об/мин
 
-    std::uint8_t coolantTemperature = 0;
-    std::uint8_t fuel = 0;
+    std::uint8_t coolantTemperature = 0;     ///< Температура охлаждающей жидкости, °C
+    std::uint8_t fuel = 0;                   ///< Уровень топлива, %
 
-    bool ignition = false;
+    bool ignition = false;                   ///< Состояние зажигания
 
-    bool leftTurn = false;
-    bool rightTurn = false;
-    bool highBeam = false;
-    bool checkEngine = false;
+    bool leftTurn = false;                   ///< Левый указатель поворота
+    bool rightTurn = false;                  ///< Правый указатель поворота
+    bool highBeam = false;                   ///< Дальний свет фар
+    bool checkEngine = false;                ///< Индикатор неисправности двигателя
 
-    bool canConnected = false;
+    bool canConnected = false;               ///< Флаг подключения к CAN-шине
 };
